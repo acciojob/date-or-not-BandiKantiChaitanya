190334898e232a3504e16d6c16954f42d5b5c00a
BandiKantiChaitanya <bandikantichaitanya2@gmail.com>
2025-02-15 19:24:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 var isDate = function (input) {
   //   write your code here
+	if (input instanceof Date) {
+    return !isNaN(input.getTime());  // Valid if it's not an invalid date
+  }
+	return (/^\d{4}-\d{2}-\d{2}$/.test(input))
 };
 
 // Do not change the code below.
-const input = prompt("Enter Date.");
+const input = prompt("Enter Date."); 
 alert(isDate(input));
